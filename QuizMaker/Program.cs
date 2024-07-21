62a0cf83888baf9556657f653d786d1b7c9dd020
jshlomov <jonatanshv@gmail.com>
2024-07-21 12:44:12 +0300

    almast final

--- QuizMaker/Program.cs
+++ QuizMaker/Program.cs
@@ -16,23 +16,28 @@
                 {
                     case 1:
                         Console.WriteLine("Type your question");
+                        quesAndAns.Question = Console.ReadLine();
+                        Console.WriteLine("Type the answer");
+                        quesAndAns.Answer = Console.ReadLine();
+                        break;
+                    case 2:
+                        List<QuesAndAns> list = xml.GetListOfQuesAndAns();
+                        foreach (var item in list)
+                        {
+                            int i = 1;
+                            Console.WriteLine(i++ + ". " + item.Question);
+                        }
+                        Console.WriteLine("Choose question");
+                        int index = int.Parse(Console.ReadLine());
+                        Console.WriteLine("What is your answer (to exit press 0)");
+                        while()
 
+
+                        break;
                     default:
                         break;
                 }
             }
-            List<QuesAndAns> list = xml.GetListOfQuesAndAns();
-            foreach (var item in list)
-            {
-                Console.WriteLine(item.Question);
-                Console.WriteLine(item.Answer);
-
-            }
-        }
-
-        public void CreateQueAndAns()
-        {
-
         }
     }
 }
